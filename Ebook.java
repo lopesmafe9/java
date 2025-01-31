4ae8e4d6d3f3ce329148274095f96be7f1bc74ff
mafê <134291043+lopesmafe9@users.noreply.github.com>
2025-01-31 17:43:02 -0300

    Add files via upload

---
+++ Ebook.java
@@ -0,0 +1,14 @@
+package com.mycompany.livro;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mariafernanda.sousa
+ */
+public class Ebook {
+    
+}
---
+++ Livro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.livro;
+
+/**
+ *
+ * @author mariafernanda.sousa
+ */
+public class Livro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
